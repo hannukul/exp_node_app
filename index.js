3dc7228d6165c4e4386fc3ec24169f15d84e7c01
Hannu Kulmala <hannukul@student.uef.fi>
2025-06-07 13:09:33 +0300

    Add more text to msg

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-  res.send('<h1>This change works yo!</h1>')
+  res.send('<h1>Hello World! This shit rocks yo!</h1>')
 })
 
 const PORT = 8080
